40ffd1e17603bcff3a0e25131473d1a841400b0f
root <root@DESKTOP-2773EJ0.localdomain>
2022-11-19 02:13:07 +0900

    1119

--- 2211/1389/1389.cpp
+++ 2211/1389/1389.cpp
@@ -5,33 +5,34 @@
 using namespace std;
 
 void	input_data(void);
-void	bacon(void);
 void	print_graph(vector<vector<int>> &graph);
+int		find_min_bacon(void);
+int		sum_of_bacon(int now_node);
+int 	bfs(int	now_node, int target_node);
 
 vector<vector<int>>	graph;
-vector<bool>		is_visited;
+vector<int>			node_depth;
+queue<int>			bfs_queue;
+int					node, edge;
 
 int main(void)
 {
-	
-	queue<int>			q;
-
 	input_data();
-	print_graph(graph);
-	//bacon();
+	//print_graph(graph);
+	int	result = find_min_bacon();
+	cout << result;
 	return (0);
 }
 
 void input_data(void)
 {
-	int	v, e;
-	cin >> v >> e;
+	cin >> node >> edge;
 
-	graph.assign(v + 1, vector<int>(0));
-	is_visited.assign(v + 1, 0);
+	graph.assign(node + 1, vector<int>(0));
+	node_depth.assign(node + 1, 0);
 
 	int i = -1;
-	while (++i < e)
+	while (++i < edge)
 	{
 		int x, y;
 		cin >> x >> y;
@@ -41,16 +42,85 @@ void input_data(void)
 	return ;
 }
 
-void print_graph(vector<vector<int>> *graph)
+void print_graph(vector<vector<int>> &graph)
 {
 	int i = 0;
-	while (++i < (*graph).size())
+
+	while (++i < graph.size())
 	{
 		int j = -1;
 		cout << i << ": ";
-		while (++j < (*graph)[i].size())
-			cout << (*graph)[i][j] << " ";
+		while (++j < graph[i].size())
+			cout << graph[i][j] << " ";
 		cout << endl;
 	}
 }
 
+int	find_min_bacon(void)
+{
+	int	min_bacon, now_node, min_node;
+
+	now_node = 1;
+	min_bacon = sum_of_bacon(now_node);
+	min_node = 1;
+	while (++now_node < node + 1)
+	{
+		int	sum;
+
+		sum = sum_of_bacon(now_node);
+		if (min_bacon > sum)
+		{
+			min_bacon = sum;
+			min_node = now_node;
+		}
+	}
+	return (min_node);
+}
+
+int	sum_of_bacon(int now_node)
+{
+	int	sum, target_node;
+
+	sum = 0;	
+	target_node = 0;
+	while (++target_node < node + 1)
+	{
+		if (now_node != target_node)
+		{
+			//cout << "@@@bfs start sum = " << sum << endl;
+			sum += bfs(now_node, target_node);
+			//cout << "@@@bfs end sum = " << sum << endl << endl;
+			fill(node_depth.begin(), node_depth.end(), 0);
+			while (!bfs_queue.empty())
+				bfs_queue.pop();
+		}
+	}
+	return (sum);
+}
+
+int bfs(int	now_node, int target_node)
+{
+	bfs_queue.push(now_node);
+	node_depth[now_node] = 1;
+
+	//cout << "@@@now: " << now_node << " target: " << target_node << endl;
+	while(1)
+	{
+		int	i = -1;
+		now_node = bfs_queue.front();
+		bfs_queue.pop();
+		while (++i < graph[now_node].size())
+		{
+			int	next_node = graph[now_node][i];
+			if (next_node == target_node)
+				return (node_depth[now_node]);
+			if (next_node > 0 && node_depth[next_node] == 0)
+			{
+				bfs_queue.push(next_node);
+				//cout << "@@@now: " << now_node << " next: " << next_node << endl;
+				node_depth[next_node] = node_depth[now_node] + 1;
+			}
+		}
+	}
+	return(-1);
+}
