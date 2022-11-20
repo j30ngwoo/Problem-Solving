2c8f42c81e5cadea4daad9436467aedbe27008f8
root <root@DESKTOP-2773EJ0.localdomain>
2022-11-20 15:02:53 +0900

    221120

---
+++ dynamic_programming/12865/12865.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[100001] = {0, };
+int weight[100];
+int value[100];
+int	n, max_weight;
+
+void	input_data(void);
+int		find_max_value(void);
+void	print_arr(void);
+
+int	main(void)
+{
+	input_data();
+	int result = find_max_value();
+	cout << result;
+
+	return (0);
+}
+
+void input_data(void)
+{
+	cin >> n >> max_weight;
+	
+	int i = -1;
+	while (++i < n)
+	{
+		int	w, v;
+		cin >> w >> v;
+		weight[i] = w;
+		value[i] = v;
+	}
+
+	return ;
+}
+
+int	find_max_value(void)
+{
+	int	i = -1;
+	while (++i < n)
+	{
+		int check_weight = max_weight + 1;
+		while (--check_weight >= 1)
+			if (check_weight >= weight[i])
+				dp[check_weight] = max(dp[check_weight], dp[check_weight - weight[i]] + value[i]);
+		print_arr();
+	}
+
+	return (dp[max_weight]);
+}
+
+void print_arr(void)
+{
+	int i = 0;
+	while (++i < max_weight + 1)
+		cout << dp[i] << " ";
+	cout << endl;
+
+	return ;
+}
