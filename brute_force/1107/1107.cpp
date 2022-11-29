d3510ed3bf7417f3d8c009cccc79c14724d15e88
j30ngwoo <ttangle1234@naver.com>
2022-11-30 00:35:28 +0900

    20221130

--- brute_force/1107/1107.cpp
+++ brute_force/1107/1107.cpp
@@ -6,8 +6,10 @@ using namespace std;
 void	input_data(void);
 int		remote_con(void);
 int		find_min(void);
+int		check_broken(int channel);
+int		check_digit(int channel);
 
-int goal;
+int goal, m;
 vector<int> broken(0);
 
 int main(void)
@@ -20,15 +22,15 @@ int main(void)
 
 void input_data(void)
 {
-	int m;
-
 	cin >> goal;
 	cin >> m;
-	while (m-- > 0)
+
+	int i = -1;
+	while (++i < m)
 	{
-		int i;
-		cin >> i;
-		broken.push_back(i);
+		int j;
+		cin >> j;
+		broken.push_back(j);
 	}
 
 	return ;
@@ -52,9 +54,49 @@ int find_min(void)
 
 	while (++channel <= 1000000)
 	{
+		if (check_broken(channel) == 0)
+			continue;
 
+		int now_push = abs(channel - goal) + check_digit(channel);
+		if (now_push < min_push)
+			min_push = now_push;
 	}
 
+	return (min_push);
+}
 
+int check_broken(int channel)
+{
+	while (channel >= 0)
+	{
+		int units = channel % 10;
+		int i = -1;
+		while (++i < m)
+		{
+			if (units == broken[i])
+				return (0);
+		}
+		channel /= 10;
 
+		if (channel == 0)
+			channel--;
+	}
+
+	return (1);
+}
+
+int check_digit(int channel)
+{	
+	int count = 0;
+
+	if (channel == 0)
+		return (1);
+
+	while (channel > 0)
+	{
+		channel /= 10;
+		count++;
+	}
+
+	return (count);
 }
