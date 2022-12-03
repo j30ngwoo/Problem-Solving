a6edfa2110f48cc87b3dc1e408d867f014c9feb2
j30ngwoo <ttangle1234@naver.com>
2022-12-03 15:12:07 +0900

    10942 complete

--- dynamic_programming/10942/10942_mk1.cpp
+++ dynamic_programming/10942/10942_mk1.cpp
@@ -7,7 +7,6 @@ void palindrome(void);
 void print_result(void);
 int is_palindrome(int index);
 void print_square(void);
-void check_all_index(void);
 
 int size_of_sequence, number_of_questions;
 pair<int, int> questions[1000000];
@@ -18,7 +17,7 @@ int result[1000000];
 int main(void)
 {
 	ios::sync_with_stdio(false);
-
+    
 	input_data();
 	palindrome();
 	print_result();
@@ -44,15 +43,13 @@ void input_data(void)
 		questions[i] = make_pair(a, b);
 	}
 
-	fill(check_palindrome[0], check_palindrome[2000], 0);
+	fill(check_palindrome[0], check_palindrome[2000], -1);
 
 	return;
 }
 
 void palindrome(void)
 {
-	check_all_index();
-
 	int i = -1;
 	while (++i < number_of_questions)
 		result[i] = is_palindrome(i);
@@ -69,8 +66,30 @@ int is_palindrome(int index)
 
 	if (check_palindrome[front_index][back_index] == 1) // is palindrome
 		return (1);
-	else // is not palindrome
+
+	else if (check_palindrome[front_index][back_index] == 0) // is not palindrome
 		return (0);
+
+	else // unknown
+	{
+		int i = -1;
+		int half_of_diff = (back_index - front_index) / 2;
+		while (++i <= half_of_diff)
+		{
+			if (sequences[front_index + i] != sequences[back_index - i])
+			{
+				check_palindrome[front_index][back_index] = 0; // when is not palindrome
+				return (0);
+			}
+		}
+		// when is palindrome
+		i = -1;
+		while (++i <= half_of_diff)
+			check_palindrome[front_index + i][back_index - i] = 1;
+		return (1);
+	}
+
+	return (-1);
 }
 
 void print_result(void)
@@ -96,28 +115,4 @@ void print_square(void)
 	cout << '\n';
 
 	return;
-}
-
-void check_all_index(void)
-{
-	int now_index = 0;
-
-	while (++now_index <= size_of_sequence)
-	{
-		check_palindrome[now_index][now_index] = 1;
-
-		int down_index = now_index;
-		int up_index = now_index;
-		while (--down_index >= 1 && ++up_index <= size_of_sequence 
-			&& sequences[down_index] == sequences[up_index])
-				check_palindrome[down_index][up_index] = 1;
-		
-		down_index = now_index + 1;
-		up_index = now_index;
-		while (--down_index >= 1 && ++up_index <= size_of_sequence
-			&& sequences[down_index] == sequences[up_index])
-				check_palindrome[down_index][up_index] = 1;
-	}
-
-	return ;
 }
