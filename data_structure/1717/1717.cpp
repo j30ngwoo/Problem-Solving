d4fce94edbc7be42333c653f44ff75a136c457a2
j30ngwoo <ttangle1234@naver.com>
2022-12-21 19:14:04 +0900

    solving 1717

---
+++ data_structure/1717/1717.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+
+
+int main(void)
+{
+	set_io();
+	input_data();
+
+
+	return (0);
+}
+
+void set_io(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	return ;
+}
+
+void input_data(void)
+{
+	cin >> n >> m;
+
+	return ;
+}
