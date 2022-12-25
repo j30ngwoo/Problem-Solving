e8d0afbd31a9f9a10f5810d7ab0b8e0e49f337dd
j30ngwoo <ttangle1234@naver.com>
2022-12-25 14:03:57 +0900

    1717 complete...?

--- data_structure/1717/1717.cpp
+++ data_structure/1717/1717.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 int n, m;
+int parent[1000001];
 
+void	set_io(void);
+void	input_data(void);
+void	union_find(void);
+int		find_parent(int child);
+void	merge(int a, int b);
+void	is_in_union(int a, int b);
 
 
 int main(void)
 {
 	set_io();
 	input_data();
-
+	union_find();
 
 	return (0);
 }
@@ -26,6 +33,55 @@ void set_io(void)
 void input_data(void)
 {
 	cin >> n >> m;
+	
+	int i = -1;
+	while (++i <= n)
+		parent[i] = i;
 
+	return ;
+}
+
+void union_find(void)
+{
+	int calculation, a, b;
+
+	int i = -1;
+	while (++i < m)
+	{
+		cin >> calculation >> a >> b;
+		if (calculation == 0)
+			merge(a, b);
+		else if (calculation == 1)
+			is_in_union(a, b);
+	}
+
+	return ;
+}
+
+int find_parent(int child)
+{
+	if (parent[child] != child)
+		find_parent(parent[child]);
+	return (child);
+}
+
+void merge(int a, int b)
+{
+	int	a_parent = find_parent(a);
+	int	b_parent = find_parent(b);
+
+	if (a_parent != b_parent)
+		parent[a] = b_parent;
+
+	return ;
+}
+
+void is_in_union(int a, int b)
+{
+	if (find_parent(a) == find_parent(b))
+		cout << "YES" << '\n';
+	else
+		cout << "NO" << '\n';
+	
 	return ;
 }
