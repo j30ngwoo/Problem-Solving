cd850d44c51b8eb6a4eb2cb881ee652ad9e380a2
j30ngwoo <ttangle1234@naver.com>
2023-01-13 22:01:23 +0900

    2042 clear

--- data_structure/2042/2042.cpp
+++ data_structure/2042/2042.cpp
@@ -4,16 +4,31 @@ using namespace std;
 
 void input_data(void);
 long long init(int, int, int);
+void update(int, long long, int, int, int);
+long long sum(int, int, int, int, int);
 
-int n, m, k;
-int arr[1000000];
-long long tree[4000000];
+int n, m, k, count;
+long long arr[1000001];
+long long tree[4000004];
 
 int main(void)
 {
 	input_data();
-	init
-
+	init(1, 1, n);
+	while (count-- > 0)
+	{
+		long long a, b, c;
+		cin >> a >> b >> c;
+		if (a == 1)
+		{
+			long long difference = c - arr[b];
+			arr[b] = c;
+			update(b, difference, 1, 1, n);
+		}
+		else
+			cout << sum(b, c, 1, 1, n) << '\n';
+	}
+	
 	return (0);
 }
 
@@ -24,9 +39,10 @@ void input_data(void)
 
 	cin >> n >> m >> k;
 
-	int i = -1;
-	while (++i < n)
+	int i = 0;
+	while (++i <= n)
 		cin >> arr[i];
+	count = m + k;
 
 	return ;
 }
@@ -34,9 +50,36 @@ void input_data(void)
 long long init(int node, int start, int end)
 {
 	if (start == end)
-		return (arr[start]);
+		return (tree[node] = arr[start]);
 
 	int mid = (start + end) / 2;
 	tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
 	return (tree[node]);
+}
+
+void update(int target_index, long long difference, int node, int start, int end)
+{
+	if (target_index < start || end < target_index)
+		return ;
+	tree[node] += difference;
+	if (start != end)
+	{
+		int mid = (start + end) / 2;
+		update(target_index, difference, node * 2, start, mid);
+		update(target_index, difference, node * 2 + 1, mid + 1, end);
+	}	
+
+	return ;
+}
+
+long long sum(int range_left, int range_right, int node, int tree_start, int tree_end)
+{
+	if (tree_end < range_left || range_right < tree_start)
+    	return (0);
+
+	if (range_left <= tree_start && tree_end <= range_right)
+    	return (tree[node]);
+
+	int mid = (tree_start + tree_end) / 2;
+	return (sum(range_left, range_right, node * 2, tree_start, mid) + sum(range_left, range_right, node * 2 + 1, mid + 1, tree_end));
 }
