2ebf4789d102457059ceef4a8efc91201cb434ea
j30ngwoo <ttangle1234@naver.com>
2023-01-15 22:07:31 +0900

    27210 clear

--- dynamic_programming/27210/27210.cpp
+++ dynamic_programming/27210/27210.cpp
@@ -2,9 +2,39 @@
 
 using namespace std;
 
+int n, left_max, right_max, total_max, input;
+
+void find_maximum(void);
+
 int main(void)
 {
-	
+	find_maximum();
+
+	return (0);
 }
 
-void 
+void find_maximum(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	int i = -1;
+	while (++i < n)
+	{
+		cin >> input;
+		input == 1 ? left_max += 1 : left_max -= 1;
+		input == 2 ? right_max += 1 : right_max -= 1;
+		if (left_max > total_max || right_max > total_max)
+			total_max = max(left_max, right_max);
+		if (left_max < 0)
+			left_max = 0;
+		if (right_max < 0)
+			right_max = 0;
+	}
+
+	cout << total_max;
+
+	return ;
+}
