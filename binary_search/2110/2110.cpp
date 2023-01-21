cf666a9a7877190393170781bded11ed3cb2b5cf
j30ngwoo <ttangle1234@naver.com>
2023-01-22 01:59:37 +0900

    2110 clear

--- binary_search/2110/2110.cpp
+++ binary_search/2110/2110.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 void input_data(void);
 void find_max(void);
-long long now_sum(int cut);
 
-int n, m, result;
-int trees[1000000];
+int n, c, result;
+int house[200000];
 
 int main(void)
 {
@@ -22,25 +22,38 @@ void input_data(void)
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> m;
-	
+	cin >> n >> c;
+
 	int i = -1;
 	while (++i < n)
-		cin >> trees[i];
+		cin >> house[i];
+
+	sort(house, house + n);
 
 	return ;
 }
 
 void find_max(void)
 {
-	int first = 0, last = 1000000000;
+	int first = 0, last = house[n - 1] - house[0];
 
 	while (first <= last)
 	{
 		int mid = (first + last) / 2;
-		long long sum = now_sum(mid);
 
-		if (sum >= m)
+		int count = 1, last_coordinate = house[0];
+		int i = 0;
+
+		while (++i < n && count < c)
+		{
+			if (house[i] - last_coordinate >= mid)
+			{
+				count++;
+				last_coordinate = house[i];
+			}
+		}
+
+		if (count == c)
 		{
 			result = mid;
 			first = mid + 1;
@@ -52,16 +65,4 @@ void find_max(void)
 	cout << result << '\n';
 
 	return ;
-}
-
-long long now_sum(int cut)
-{
-	long long sum = 0;
-
-	int i = -1;
-	while (++i < n)
-		if (trees[i] - cut > 0)
-			sum += trees[i] - cut;
-
-	return (sum);
 }
