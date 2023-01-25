aa62459c39a9c90e6678f2276ac89e87052c734f
j30ngwoo <ttangle1234@naver.com>
2023-01-26 03:05:58 +0900

    9935 clear

--- data_structure/9935/9935.cpp
+++ data_structure/9935/9935.cpp
@@ -2,12 +2,18 @@
 
 using namespace std;
 
+void explosion(void);
+
 string str, frula, result;
 
 int main(void)
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+
+	str.reserve(1000001);
+	frula.reserve(40);
+	result.reserve(1000001);
 	
 	explosion();
 
@@ -21,18 +27,39 @@ void explosion(void)
 
 	int i = -1;
 	int result_index = -1;
-	
+	int result_size = str.size();
+	int last_result_index;
+
 	while (++i < str.size())
 	{
-		int frula_index = frula.size() + 1;
+		int frula_index = frula.size() - 1;
 		result[++result_index] = str[i];
-		while (flula[--frula_index] == result[result_index] && result_index >= 0)
+		last_result_index = result_index;
+		while (last_result_index >= 0 && frula_index >= 0)
 		{
-			
+			if (result[last_result_index] == frula[frula_index])
+			{
+				frula_index--;
+				last_result_index--;
+			}
+			else
+				break;
+		}
+		if (frula_index == -1)
+		{
+			result_size -= frula.size();
+			result_index = last_result_index;
 		}
 	}
 
-	cout << result;
+	if (result_size == 0)
+		cout << "FRULA";
+	else
+	{
+		i = -1;
+		while (++i < result_size)
+			cout << result[i];
+	}
 
 	return ;
 }
