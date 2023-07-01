ea659187079774473d71ef5010e803b6da3d35c3
j30ngwoo <ttangle1234@naver.com>
2023-07-01 12:35:55 +0900

    11725 clear

---
+++ graph_theory/11725/11725.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n, parent[100001];
+vector<int> graph[100001];
+queue<int> q;
+
+int main(void)
+{
+	cin >> n;
+	for (int i = 0; i < n - 1; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	
+	q.push(1);
+	parent[1] = 1;
+	while (!q.empty())
+	{
+		int now_node = q.front();
+		q.pop();
+		for (int i = 0; i < graph[now_node].size(); i++)
+		{
+			int next_node = graph[now_node].at(i);
+			if (parent[next_node] == 0)
+			{
+				parent[next_node] = now_node;
+				q.push(next_node);
+			}
+		}
+	}
+
+	for (int i = 2; i <= n; i++)
+		cout << parent[i] << '\n';
+
+	return (0);
+}
