88ddc99ed9769a45409d0582ae02a64d0a8cd446
j30ngwoo <ttangle1234@naver.com>
2023-12-27 13:16:38 +0900

    solving 20652

--- implementation/20652/20652.cpp
+++ implementation/20652/20652.cpp
@@ -1,51 +1,79 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define INFINITY 1000000001
+#define INF 1000000001
 #define FOR(i, n) for(int i = 0; i < n; i++)
 
 using namespace std;
 
-class Cow {
-	public:
-		char direction;
-		int x;
-		int y;
+struct Cow {
+	int x;
+	int y;
+	int id;
 };
 
-class Intersect {
-	public:
-		Cow stop_cow;
-		Cow continue_cow;
-		int time;
-		int x;
-		int y;
+struct Stop {
+	int x;
+	int y;
 };
 
-vector<Cow> cows;
-vector<Intersect> intersects;
-int n;
+bool ascendingX(Cow a, Cow b) {
+	return a.x < b.x;
+}
+
+bool ascendingY(Cow a, Cow b) {
+	return a.y < b.y;
+}
+
+vector<Cow> eastCows;
+vector<Cow> northCows;
+vector<Stop> stops(50, {0, 0});
+vector<int> result(50, INF);
 
 int main(void) {
+	int n;
 	cin >> n;
 	FOR(i, n) {
 		char direction;
 		int x, y;
 		cin >> direction >> x >> y;
-		cows.push_back({direction, x, y});
+		if (direction == 'E') eastCows.push_back({x, y, i});
+		if (direction == 'N') northCows.push_back({x, y, i});
 	}
-	
-	FOR(a, n) {
-		FOR(b, n) {
-			if (cows[a].direction == 'E' && cows[b].direction == 'N' && cows[a].x < cows[b].x && cows[a].y > cows[b].y) {
-				if (cows[b].x - cows[a].x < cows[a].y - cows[b].y)
-					intersects.push_back({cows[b], cows[a], cows[]});
 
-				else if (cows[])
+	sort(northCows.begin(), northCows.end(), ascendingX); // northCow는 x좌표 오름차순
+	sort(eastCows.begin(), eastCows.end(), ascendingY); // eastCow는 y좌표 오름차순
+	
+	// 각 northCow에 대한 eastCow의 충돌 확인
+	for (auto northCow: northCows) {
+		for (auto eastCow: eastCows) {
+			if (eastCow.x < northCow.x && northCow.y < eastCow.y) {
+				int eastDist = northCow.x - eastCow.x;
+				int northDist = eastCow.y - northCow.y;
+				if (northDist < eastDist && stops[eastCow.id].x == 0) { // eastCow가 정지 && 이전의 northCow에서 정지한 적 없음
+					stops[eastCow.id] = ({northCow.y, eastCow.x});         // eastCow의 정지 위치 표시
+				} 
+				if (eastDist < northDist && stops[eastCow.id].x == 0) { // northCow가 정지 && 겹친 eastCow는 이전에 정지한 적 없음
+					stops[northCow.id] = ({northCow.y, eastCow.x});      // northCow의 정지 위치 표시. 정지하였으므로 다음 northCow로 넘어감.
+					break ;
+				}
 			}
 		}
 	}
 
+	for (auto eastCow: eastCows)
+		if (stops[eastCow.id].x != 0)
+			result[eastCow.id] = stops[eastCow.id].y - eastCow.y;
+	for (auto northCow: northCows)
+		if (stops[northCow.id].x != 0)
+			result[northCow.id] = stops[northCow.id].y - northCow.y;
+	
+	for (auto val: result) {
+		if (val == INF)
+			cout << "Infinity\n";
+		else
+			cout << val << '\n';
+	}
 
 	return (0);
 }
