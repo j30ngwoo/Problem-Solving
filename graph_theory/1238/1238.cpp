b6fadc78cb0bbd75dcaf8999032006fecf7a03dc
j30ngwoo <ttangle1234@naver.com>
2023-12-29 04:59:58 +0900

    1238 clear

--- graph_theory/1238/1238.cpp
+++ graph_theory/1238/1238.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 #define FOR(i, n) for(int i = 0; i < n; i++)
 #define FOR1(i, n) for(int i = 1; i <= n; i++)
 #define INF INT32_MAX
@@ -10,65 +11,58 @@ using namespace std;
 struct Node {
 	int dist;
 	int id;
+	bool operator<(const Node &d) const {
+		return dist > d.dist;
+	}
 };
 
-int homeToX[1001];
-int xToHome[1001];
-bool isMinFound[1001];
-priority_queue<Node, vector<Node>, greater<Node>> pq;
-
+priority_queue<Node> pq;
 int n, m, x;
 
 int main(void) {
-	cin >> n >> m >> x;
-	vector<vector<Node>> graph(n + 1);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-	FOR1(home, n) {
+	cin >> n >> m >> x;
+	vector<int> homeToX(n + 1, INF);
+	vector<int> xToHome(n + 1, INF);
+	vector<vector<Node>> graph(n + 1);
+	vector<vector<Node>> revGraph(n + 1);
+
+	FOR1(home, m) {
 		int begin, end, time;
 		cin >> begin >> end >> time;
-		graph[begin].push_back({end, time});
-	}
-	
-	FOR1(home, n) {
-		fill_n(isMinFound, isMinFound + 1001, false);
-		while(!pq.empty())
-			pq.pop();
-		pq.push({0, home});
-		isMinFound[home] = true;
-		while(!pq.empty()) {
-			int id = pq.top().id;
-			int dist = pq.top().dist;
-			pq.pop();
-			if (isMinFound[id]) continue;
-			isMinFound[id] = true;
-			if (id == x) {
-				homeToX[id] = dist;
-				break;
-			}
-			for (Node nextNode : graph[id]) {
-				pq.push({dist + nextNode.dist, nextNode.id});
-			}
-		}
-	}
+		graph[begin].push_back({time, end});
+		revGraph[end].push_back({time, begin});
+	}	
 
-	fill_n(isMinFound, isMinFound + 1001, false);
-	while(!pq.empty())
-		pq.pop();
 	pq.push({0, x});
-	isMinFound[x] = true;
 	while(!pq.empty()) {
 		int id = pq.top().id;
 		int dist = pq.top().dist;
 		pq.pop();
-		if (isMinFound[id]) continue;
-		isMinFound[id] = true;
+		if (xToHome[id] < dist) continue;
 		xToHome[id] = dist;
 		for (Node nextNode : graph[id]) {
+			if (xToHome[nextNode.id] < nextNode.dist) continue;
+			pq.push({dist + nextNode.dist, nextNode.id});
+		}
+	}
+	
+	pq.push({0, x});
+	while(!pq.empty()) {
+		int id = pq.top().id;
+		int dist = pq.top().dist;
+		pq.pop();
+		if (homeToX[id] < dist) continue;
+		homeToX[id] = dist;	
+		for (Node nextNode : revGraph[id]) {
+			if (homeToX[nextNode.id] < nextNode.dist) continue;
 			pq.push({dist + nextNode.dist, nextNode.id});
 		}
 	}
 
-	int maximum = -1;
+	int maximum = 0;
 	FOR1(i, n)
 		maximum = max(maximum, homeToX[i] + xToHome[i]);
 	cout << maximum;
