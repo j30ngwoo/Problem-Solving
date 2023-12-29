4e581f1c86a84f941c5ac842b398f7d9fbb81232
j30ngwoo <ttangle1234@naver.com>
2023-12-29 15:06:21 +0900

    solving 17128

---
+++ dynamic_programming/17128/17128.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Accum {
+	string s;
+	int
+string s1, s2;
+string table[41][41];
+int result = 0;
+
+int main(void) {
+	cin >> s1 >> s2;
+	s1.insert(0, "X");
+	s2.insert(0, "X");
+
+	for (int y = 1; y < s1.length(); y++) {
+		for (int x = 1; x < s2.length(); x++) {
+			
+		}
+	}
+	cout << result;
+
+	return (0);
+}
