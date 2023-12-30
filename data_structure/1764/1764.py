45fbc965c94cae8a49f8563266a301232f779cf5
j30ngwoo <ttangle1234@naver.com>
2023-12-30 14:56:35 +0900

    1764 clear

--- data_structure/1764/1764.py
+++ data_structure/1764/1764.py
@@ -1,12 +1,10 @@
 import sys
 input = sys.stdin.readline.rstrip
 print = sys.stdout.write
-
 a = set()
 b = set()
 
 n, m = map(int, input().split()) 
-
 for _ in range(n):
 	a.add(input())
 for _ in range(m):
