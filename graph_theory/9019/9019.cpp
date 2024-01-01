c7e8759cd4f6068b3a0570efb4a37988746d1e56
j30ngwoo <ttangle1234@naver.com>
2024-01-01 11:37:56 +0900

    9019 clear

--- graph_theory/9019/9019.cpp
+++ graph_theory/9019/9019.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <functional>
 #include <queue>
-#include <unistd.h>
+#include <string>
 #define FOR(i, n) for(int i = 0; i < n; i++)
 #define FOR1(i, n) for(int i = 1; i <= n; i++)
 using namespace std;
 
 struct E {
 	int num;
-	vector<char> commandStr;
+	string command;
 };
 
 int D(int num) {
@@ -30,11 +30,8 @@ int R(int num) {
 	return (num / 10 + (num % 10) * 1000);
 }
 
-function<int(int)> commands[4] = {D, S, L, R};
-char DSLR[4] = {'D', 'S', 'L', 'R'};
-bool isVisited[10000];
-queue<E> q;
-vector<vector<char>> result;
+function<int(int)> func[4] = {D, S, L, R};
+string DSLR[4] = {"D", "S", "L", "R"};
 int t;
 
 int main(void) {
@@ -42,33 +39,28 @@ int main(void) {
 	cin.tie(0);
 	cin >> t;
 
-	FOR(i, t) {
+	FOR(x, t) {
 		int a, b;
 		cin >> a >> b;
-		fill_n(isVisited, 10000, false);
-		while (!q.empty())
-			q.pop();
-		q.push({a, vector<char>()});
+		bool isVisited[10000] = {0,};
+		queue<E> q;
+		q.push({a, ""});
+		isVisited[a] = true;
 		while (!q.empty()) {
-			int num = q.front().num;
-			vector<char> commandStr = q.front().commandStr;
+			int nowNum = q.front().num;
+			string nowCommand  = q.front().command;
 			q.pop();
-			if (isVisited[num]) continue;
-			isVisited[num] = true;
-			if (num == b) { 
-				result.push_back(commandStr);
+			if (nowNum == b) {
+				cout << nowCommand << '\n';
 				break ;
-			}	
-			FOR(i, 4) {
-				int nextNum = commands[i](num);
-				if (isVisited[nextNum] || nextNum < 0 || 10000 <= nextNum) continue;
-				q.push({nextNum, commandStr.push_back(DSLR[i])});
-				//cout << nextNum << '\n';
 			}
-		}
+			FOR (i, 4) {
+				int nextNum = func[i](nowNum);
+				if (isVisited[nextNum]) continue;
+				isVisited[nextNum] = true;
+				q.push({nextNum, nowCommand + DSLR[i]});
+			}
+		} 
 	}
-	
-	for (auto elem : result)
-		cout << elem << '\n';
 	return (0);
 }
