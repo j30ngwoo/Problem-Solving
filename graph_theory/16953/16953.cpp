679c17c62ff9042cd76c41dac0ff17a9f0a1c559
j30ngwoo <ttangle1234@naver.com>
2024-01-14 17:03:19 +0900

    modify 16953

--- graph_theory/16953/16953.cpp
+++ graph_theory/16953/16953.cpp
@@ -27,13 +27,10 @@ int main(void) {
 			cout << nowCount;
 			return (0);
 		}
-		ll nextCount = nowCount + 1;
-		ll nextN = nowN * 10 + 1;
-		if (nextN <= 10000000000)
-			q.push({nextN, nextCount});
-		nextN = nowN * 2;
-		if (nextN <= 1000000000)
-			q.push({nextN, nextCount});
+		if (nowN * 10 + 1 <= 10000000000)
+			q.push({nowN * 10 + 1, nowCount + 1});
+		if (nowN * 2 <= 1000000000)
+			q.push({nowN * 2, nowCount + 1});
 	}
 	cout << -1;
 	return (0);
