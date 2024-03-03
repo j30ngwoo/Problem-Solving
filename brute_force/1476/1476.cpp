d253e58bba75ec52e91af503d79df040bdd13e04
j30ngwoo <ttangle1234@naver.com>
2024-03-03 17:54:57 +0900

    1476 clear

---
+++ brute_force/1476/1476.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int e, s, m, i = 1;
+	cin >> e >> s >> m;
+	if (e == 15) e = 0;
+	if (s == 28) s = 0;
+	if (m == 19) m = 0;
+	while (!(i % 15 == e && i % 28 == s && i % 19 == m))
+		i++;
+	cout << i;
+	return (0);
+}
