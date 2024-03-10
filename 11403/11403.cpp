3f9dc1a344b84ab80dd7f6f71a3988c831f94756
j30ngwoo <ttangle1234@naver.com>
2024-03-10 10:41:55 +0900

    11403 clear

--- 11403/11403.cpp
+++ 11403/11403.cpp
@@ -2,15 +2,13 @@
 #define FOR(i, n) for(int i = 0; i < n; i++)
 #define FOR1(i, n) for(int i = 1; i <= n; i++)
 using namespace std;
-bool isVisited[100];
 int graph[100][100];
 int n;
 
-void recursion(int nowNode	) {
-	
-}
-
 int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
 	cin >> n;
 	FOR(from, n) {
 		FOR(to, n) {
@@ -20,10 +18,18 @@ int main(void) {
 		}
 	}
 	
-	FOR(from, n) {
-	
-	}
+	FOR(mid, n)
+		FOR(first, n)
+			FOR(last, n)
+				if (graph[first][mid] == 1 && graph[mid][last] == 1)
+					graph[first][last] = 1;
 
+	FOR(from, n) {
+		FOR(to, n) {
+			cout << graph[from][to] << ' ';
+		}
+		cout << '\n';
+	}
 
 	return (0);
 }
