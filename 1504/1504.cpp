348ba4f63d3be609ef88476c9e80851c06d72328
j30ngwoo <ttangle1234@naver.com>
2024-04-08 14:58:01 +0900

    solving 1504

--- 1504/1504.cpp
+++ 1504/1504.cpp
@@ -11,16 +11,15 @@ struct edge {
 
 struct route {
 	bool operator()(const route& a, const route& b) {
-		return a.distance > b.distance;
+		return a.totalDist > b.totalDist;
 	}
 	int node;
-	int distance;
+	int totalDist;
 };
-
-
 
 int n, e, a, b, c, V1, V2, startToV1, startToV2, V1ToV2, V1ToEnd, V2ToEnd;
 vector<edge> graph[801];
+bool isVisited[801];
 
 int main(void) {
 	ios_base::sync_with_stdio(0);
@@ -34,7 +33,24 @@ int main(void) {
 	}
 
 	priority_queue<route> pq;
-	pq.
+	pq.push({1, 0});
+	while (!pq.empty()) {
+		int nowNode = pq.top().node;
+		int totalDist = pq.top().totalDist;
+		pq.pop();
+		if (nowNode == a) {
+			startToV1 = totalDist;
+			break ;
+		}
+		for (edge nextEdge : graph[nowNode]) {
+			int nextNode = nextEdge.endNode;
+			int nextDist = nextEdge.distance; 
+			if (isVisited[nextNode])
+				continue ;
+			isVisited[nextNode] = true;
+			pq.push({nextNode, totalDist + nextDist});
+		}
+	}
 
 	cout << min(startToV1 + V1ToV2 + V2ToEnd, startToV2 + V1ToV2 + V1ToEnd);
 	return (0);
