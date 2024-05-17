deff36a990b1e8268127328e372bfd0eba4b3763
j30ngwoo <ttangle1234@naver.com>
2024-05-17 14:58:35 +0900

    15663 clear

--- backtracking/15663/15663.cpp
+++ backtracking/15663/15663.cpp
@@ -1,25 +1,31 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <string>
+#include <set>
 #define FOR(i, n) for (int i = 0; i < n; i++)
 #define FOR1(i, n) for (int i = 1; i <= n; i++)
 using namespace std;
 
 int n, m, x;
+bool isSelected[9];
 vector<int> arr;
 vector<int> result;
+set<vector<int>> resultSet;
 
 void recursion(int index, int layer) {
 	if (layer == 0) {
-		for (int e : result)
-			cout << e << ' ';
-		cout << '\n';
+		resultSet.insert(result);
 		return ;
 	}
-	for (int i = index; i <= n - layer; i++) {
+	for (int i = 0; i < n; i++) {
+		if (isSelected[i])
+			continue;
 		result.push_back(arr[i]);
+		isSelected[i] = true;
 		recursion(i + 1, layer - 1);
 		result.pop_back();
+		isSelected[i] = false;
 	}
 }
 
@@ -31,7 +37,13 @@ int main(void) {
 		arr.push_back(x);
 	}
 	sort(arr.begin(), arr.end());
-
 	recursion(0, m);
+	vector<vector<int>> resultVector(resultSet.begin(), resultSet.end());
+	sort(resultVector.begin(), resultVector.end());
+	for (vector<int> v : resultVector) {
+		for (int e : v)
+			cout << e << ' ';
+		cout << '\n';
+	}
 	return (0);
 }
