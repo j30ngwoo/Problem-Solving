defcec714ab78c371067e9798ce37e04253d7160
j30ngwoo <ttangle1234@naver.com>
2024-05-17 16:14:31 +0900

    solving 1991

---
+++ graph_theory/1991/1991.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	char value;
+	Node* left;
+	Node* right;
+};
+
+int n;
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	Node* tree = new Node({'A', nullptr, nullptr});
+	
+
+	return (0);
+}
