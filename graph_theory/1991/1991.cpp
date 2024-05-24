0d34b2664c6ef902212def9f2927057ef53689a5
j30ngwoo <ttangle1234@naver.com>
2024-05-24 14:44:14 +0900

    1991 clear

--- graph_theory/1991/1991.cpp
+++ graph_theory/1991/1991.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 struct Node {
-	char value;
-	Node* left;
-	Node* right;
+	char left;
+	char right;
 };
-
 int n;
+map<char, Node> graph;
+
+void preorder(char node) {
+	if (node == '.')
+		return ;
+	cout << node;
+	preorder(graph[node].left);
+	preorder(graph[node].right);
+}
+
+void inorder(char node) {
+	if (node == '.')
+		return ;
+	inorder(graph[node].left);
+	cout << node;
+	inorder(graph[node].right);
+}
+
+void postorder(char node) {
+	if (node == '.')
+		return ;
+	postorder(graph[node].left);
+	postorder(graph[node].right);
+	cout << node;
+}
+
 int main(void) {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	Node* tree = new Node({'A', nullptr, nullptr});
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		char node, left, right;
+		cin >> node >> left >> right;
+		graph.insert({node, {left, right}});
+	}
 	
-
+	preorder('A');
+	cout << '\n';
+	inorder('A');
+	cout << '\n';
+	postorder('A');
+	cout << '\n';
 	return (0);
 }
