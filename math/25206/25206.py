abe8b6e3ddc6025ee6788e68a4d435159f4ce1ff
j30ngwoo <ttangle1234@naver.com>
2024-06-09 16:00:17 +0900

    25206 clear

---
+++ math/25206/25206.py
@@ -0,0 +1,12 @@
+
+score = {'A+':4.5,'A0':4.0,'B+':3.5,'B0':3.0,'C+':2.5,'C0':2.0,'D+':1.5,'D0':1.0,'F':0.0}
+
+total_time = 0
+s = 0
+for _ in range(20):
+	name, time, grade = input().split()
+	if grade == 'P':
+		continue
+	total_time += float(time)
+	s += float(time) * score[grade]
+print(s / total_time)
