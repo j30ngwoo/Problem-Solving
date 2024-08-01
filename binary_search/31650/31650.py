97cfc71d21ada0047c70dd9b1be2861286b6004e
j30ngwoo <ttangle1234@naver.com>
2024-08-02 03:15:06 +0900

    31650 clear

---
+++ binary_search/31650/31650.py
@@ -0,0 +1,9 @@
+﻿from bisect import bisect_right
+import sys
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+diff = sorted([c - t for c, t in zip(list(map(int, input().split())), list(map(int, input().split())))])
+for _ in range(q):
+	v, s = map(int, input().split())
+	print("YES") if v <= n - bisect_right(diff, s) else print("NO")
