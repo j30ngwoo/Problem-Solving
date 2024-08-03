6fca2d263434bed76fa857282190348554749bb3
j30ngwoo <ttangle1234@naver.com>
2024-08-03 13:10:46 +0900

    24981 clear

---
+++ 24981/24981.py
@@ -0,0 +1,21 @@
+﻿from bisect import bisect_left, bisect_right
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+l = []
+g = []
+for _ in range(n):
+	d, v = input().split()
+	if d == 'L': l.append(int(v))
+	else: g.append(int(v))
+
+l.sort()
+g.sort()
+m = 0
+for pos in l:
+	m = max(m, bisect_right(g, pos) + (len(l) - bisect_left(l, pos)))
+for pos in g:
+	m = max(m, bisect_right(g, pos) + (len(l) - bisect_left(l, pos)))
+
+print(n - m)
