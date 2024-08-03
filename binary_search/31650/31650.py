19517e3b8213d337f2b3b808a5394d2ad6ee7389
j30ngwoo <ttangle1234@naver.com>
2024-08-03 11:52:42 +0900

    31650 clear

--- binary_search/31650/31650.py
+++ binary_search/31650/31650.py
@@ -6,4 +6,4 @@ n, q = map(int, input().split())
 diff = sorted([c - t for c, t in zip(list(map(int, input().split())), list(map(int, input().split())))])
 for _ in range(q):
 	v, s = map(int, input().split())
-	print("YES") if v <= n - bisect_right(diff, s) else print("NO")
+	print("YES") if v <= n - bisect_right(diff, s) else print("NO")\
