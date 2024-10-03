c796690694c7704932246996cfa7e321f1686aa2
j30ngwoo <ttangle1234@naver.com>
2024-10-03 10:30:30 +0900

    15751 clear

---
+++ USACO/15751/15751.py
@@ -0,0 +1,8 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+a, b, x, y = map(int, input().split())
+d1 = abs(a - b)
+d2 = abs(a - x) + abs(b - y)
+d3 = abs(a - y) + abs(b - x)
+print(min(d1, d2, d3))
