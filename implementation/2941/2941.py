3551a531633bfb5dc5c7938f5a80fdef5d24622b
j30ngwoo <ttangle1234@naver.com>
2024-10-05 18:57:32 +0900

    2914 clear

---
+++ implementation/2941/2941.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+w = input()
+l = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+count = 0
+i = 0
+
+while i < len(w):
+    found = False
+    for c_alpha in l:
+        if w[i:i+len(c_alpha)] == c_alpha:
+            i += len(c_alpha)
+            found = True
+            break
+    if not found:
+        i += 1
+    count += 1
+
+print(count)
