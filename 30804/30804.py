6535282403cd70feae00e1ac7bb64678b0ce180a
j30ngwoo <ttangle1234@naver.com>
2024-10-06 16:27:04 +0900

    30804 clear

--- 30804/30804.py
+++ 30804/30804.py
@@ -1,8 +1,22 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
 n = int(input())
-arr = list(map(int, input().split()))
-used_fruit = set()
+fruits = list(map(int, input().split()))
+used_fruits = set()
+prev_index = 0
 result = 0
+count = 0
 
-for fruit in arr:
-    if 
+for i in range(len(fruits)):
+    if len(used_fruits) >= 2 and fruits[i] not in used_fruits:
+        used_fruits.discard(fruits[prev_index])
+        count = i - prev_index
+    else:
+        count += 1
+    if fruits[i] != fruits[i - 1]:
+            prev_index = i - 1
+    used_fruits.add(fruits[i])
+    result = max(result, count)
 
+print(result)
