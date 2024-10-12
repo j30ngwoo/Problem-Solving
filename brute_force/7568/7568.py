f78f70ff12a0c1d0ded8b07efb5775d0c3fb4502
j30ngwoo <ttangle1234@naver.com>
2024-10-12 11:10:24 +0900

    7568 clear

---
+++ brute_force/7568/7568.py
@@ -0,0 +1,14 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(tuple(map(int, input().split())))
+
+for me in arr:
+    count = 1
+    for other in arr:
+        if other[0] > me[0] and other[1] > me[1]:
+            count += 1
+    print(count, end=' ')
