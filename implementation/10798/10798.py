b475057c1295661f52fbe52de624911a2d275dde
j30ngwoo <ttangle1234@naver.com>
2024-10-13 15:29:38 +0900

    10798 claer

---
+++ implementation/10798/10798.py
@@ -0,0 +1,11 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+arr = []
+for _ in range(5):
+    arr.append(input())
+
+for x in range(max(map(len, arr))):
+    for y in range(5):
+        if len(arr[y]) > x:
+            print(arr[y][x], end='')
