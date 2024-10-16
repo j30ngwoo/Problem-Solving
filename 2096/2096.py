dfc03a0b88838e1720cf241a9dc10e5450579544
j30ngwoo <ttangle1234@naver.com>
2024-10-17 01:26:03 +0900

    improved solution

--- 2096/2096.py
+++ 2096/2096.py
@@ -1,4 +1,25 @@
-﻿import sys
+﻿# import sys
+# input = lambda: sys.stdin.readline().strip()
+
+# grid_max = [0, 0, 0]
+# grid_min = [0, 0, 0]
+
+# n = int(input())
+# for i in range(n):
+#     newline_max = list(map(int, input().split()))
+#     newline_min = newline_max[:]
+#     newline_max[0] += max(grid_max[0:2])
+#     newline_max[1] += max(grid_max[0:3])
+#     newline_max[2] += max(grid_max[1:3])
+#     grid_max = newline_max[:]
+#     newline_min[0] += min(grid_min[0:2])
+#     newline_min[1] += min(grid_min[0:3])
+#     newline_min[2] += min(grid_min[1:3])
+#     grid_min = newline_min[:]
+
+# print(max(grid_max), min(grid_min))
+
+import sys
 input = lambda: sys.stdin.readline().strip()
 
 grid_max = [0, 0, 0]
@@ -6,15 +27,8 @@ grid_min = [0, 0, 0]
 
 n = int(input())
 for i in range(n):
-    newline_max = list(map(int, input().split()))
-    newline_min = newline_max[:]
-    newline_max[0] += max(grid_max[0:2])
-    newline_max[1] += max(grid_max[0:3])
-    newline_max[2] += max(grid_max[1:3])
-    grid_max = newline_max[:]
-    newline_min[0] += min(grid_min[0:2])
-    newline_min[1] += min(grid_min[0:3])
-    newline_min[2] += min(grid_min[1:3])
-    grid_min = newline_min[:]
+    newline = tuple(map(int, input().split()))
+    grid_max = [newline[0] + max(grid_max[0:2]), newline[1] + max(grid_max[0:3]), newline[2] + max(grid_max[1:3])]
+    grid_min = [newline[0] + min(grid_min[0:2]), newline[1] + min(grid_min[0:3]), newline[2] + min(grid_min[1:3])]
 
 print(max(grid_max), min(grid_min))
