df69a3186f68a8ffd94dff4f16df3c040405a5fc
j30ngwoo <ttangle1234@naver.com>
2024-10-17 00:52:29 +0900

    15666 clear

---
+++ backtracking/15666/15666.py
@@ -0,0 +1,19 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def recursion(index, layer):
+    if layer == 0:
+        print(" ".join(map(str, arr)))
+        return
+
+    for next_index in range(index, len(l)):
+        arr.append(l[next_index])
+        recursion(next_index, layer - 1)
+        arr.pop()
+
+
+n, m = map(int, input().split())
+l = sorted(list(set(map(int, input().split()))))
+
+arr = []
+recursion(0, m)
