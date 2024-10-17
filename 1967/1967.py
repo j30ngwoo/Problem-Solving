a3fb412bce0844c4edd04214a36a776ccf389d0d
j30ngwoo <ttangle1234@naver.com>
2024-10-18 00:16:02 +0900

    solving 1967

--- 1967/1967.py
+++ 1967/1967.py
@@ -1,27 +1,11 @@
 ﻿import sys
 input = lambda: sys.stdin.readline().strip()
-from queue import Queue
 
-INF = 1000000
-n, k = map(int, input().split())
-arr = [[INF, 0] for _ in range(100001)]
-q = Queue()
-q.put((n, 0))
+n = int(input())
+tree = [[] for _ in range(10001)]
+for _ in range(n):
+    p, c, w = map(int, input().split())
+    tree[p].append((c, w))
+    tree[c].append((p, w))
 
-while not q.empty():
-    now_pos, now_count = q.get()
-
-    if not (0 <= now_pos <= 100000) or arr[k][0] < now_count:
-        continue
-
-    if arr[now_pos][0] < now_count:
-        continue
-    elif now_count <= arr[now_pos][0]:
-        arr[now_pos][0] = now_count
-        arr[now_pos][1] += 1
-
-    q.put((now_pos + 1, now_count + 1))
-    q.put((now_pos - 1, now_count + 1))
-    q.put((now_pos * 2, now_count + 1))
-
-print(arr[k][0], arr[k][1])
+for 
