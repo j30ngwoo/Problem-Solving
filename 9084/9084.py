3b575f1466d1b5366c43452bad61103583fc7f21
j30ngwoo <ttangle1234@naver.com>
2024-10-19 18:32:34 +0900

    solving 9084

--- 9084/9084.py
+++ 9084/9084.py
@@ -4,14 +4,9 @@ input = sys.stdin.readline
 t = int(input())
 
 for _ in range(t):
-	dp = [1] + [0] * 10000
 	n = int(input())
 	coins = list(map(int, input().split()))
 	m = int(input())
-	for i in range(1, m + 1):
-		dp[i] = dp[i] - 1
-		for coin in coins:
-			if i - coin >= 0:
-				dp[i] += 1
-	print(dp[:m + 1])
-	print(dp[m])
+	
+	dp = 
+
