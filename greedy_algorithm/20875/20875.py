9cb885e29b2db95e3348169eeb854d8e4030e044
j30ngwoo <ttangle1234@naver.com>
2024-10-27 23:33:11 +0900

    20875 clear

---
+++ greedy_algorithm/20875/20875.py
@@ -0,0 +1,19 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+cows = list(map(int, input().split()))
+stalls = list(map(int, input().split()))
+cows.sort(reverse=True)
+
+def count_possible_stalls(h):
+    count = 0
+    for stall in stalls:
+        if h <= stall:
+            count += 1
+    return count
+
+result = 1
+for i in range(n):
+    result *= count_possible_stalls(cows[i]) - i
+print(result)
