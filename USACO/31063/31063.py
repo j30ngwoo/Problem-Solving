ba2de712eee277d7afe54ff798f89579589db79e
j30ngwoo <ttangle1234@naver.com>
2024-10-28 01:30:43 +0900

    31063 clear

---
+++ USACO/31063/31063.py
@@ -0,0 +1,18 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n, m = list(map(int, input().split()))
+cows = list(map(int, input().split()))
+canes = list(map(int, input().split()))
+
+for cane_top in canes:
+    cane_bottom = 0
+    for i in range(len(cows)):
+        if cows[i] > cane_bottom:
+            eat = min(cows[i] - cane_bottom, cane_top - cane_bottom)
+            cows[i] += eat
+            cane_bottom += eat
+            if cane_top == cane_bottom:
+                break 
+
+print('\n'.join(map(str, cows)))
