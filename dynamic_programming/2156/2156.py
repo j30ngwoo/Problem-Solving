147bed3ce3c04c05c5ac6f9890b62898e6592f44
j30ngwoo <ttangle1234@naver.com>
2024-10-29 18:02:29 +0900

    2156 clear

---
+++ dynamic_programming/2156/2156.py
@@ -0,0 +1,13 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+wines = [int(input()) for _ in range(n)]
+dp = [[0, 0, 0] for _ in range(n + 1)]
+
+for i in range(n):
+    dp[i][0] = max(dp[i - 1])
+    dp[i][1] = dp[i - 1][0] + wines[i]
+    dp[i][2] = dp[i - 1][1] + wines[i]
+
+print(max(dp[n - 1]))
