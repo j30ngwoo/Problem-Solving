70cbde3c2b74b771ea00347aba4d6c56d5ebb3c9
j30ngwoo <ttangle1234@naver.com>
2024-10-31 01:11:56 +0900

    쉬운 문제 풀이

---
+++ sort/2587/2587.py
@@ -0,0 +1,6 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+arr = sorted([int(input()) for _ in range(5)])
+print(sum(arr) // 5)
+print(arr[2])
---
+++ sort/2750/2750.py
@@ -0,0 +1,4 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+print('\n'.join(map(str, sorted([int(input()) for _ in range(int(input()))]))))
