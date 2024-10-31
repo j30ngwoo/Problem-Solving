6ae9e02fb7f776287fba0c75852baa7b886d501f
j30ngwoo <ttangle1234@naver.com>
2024-10-31 15:17:34 +0900

    2559 clear

---
+++ sliding_window/2559/2559.py
@@ -0,0 +1,13 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+s = sum(arr[0:k])
+result = s
+for i in range(n - k):
+    s = s - arr[i] + arr[i + k]
+    result = max(result, s)
+
+print(result)
