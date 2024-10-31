7b3a9db401d2e75e6d9706cd8ab5017c1471559c
j30ngwoo <ttangle1234@naver.com>
2024-11-01 01:11:13 +0900

    11054 clear

---
+++ dynamic_programming/11054/11054.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+ascending = [1] * n
+for now in range(n):
+    for prev in range(now):
+        if arr[prev] < arr[now]:
+            ascending[now] = max(ascending[now], ascending[prev] + 1)
+
+descending = [1] * n
+for now in range(n - 1, -1, -1):
+    for next in range(now + 1, n):
+        if arr[now] > arr[next]:
+            descending[now] = max(descending[now], descending[next] + 1)
+
+result = [a + d for a, d in zip(ascending, descending)]
+print(max(result) - 1)
