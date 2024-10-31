4b8a5c00f8c1026a9e3041a4504d5cd2fb398e6e
j30ngwoo <ttangle1234@naver.com>
2024-10-31 22:55:02 +0900

    25192 clear

---
+++ data_structure/25192/25192.py
@@ -0,0 +1,17 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+set_ = set()
+result = 0
+
+for _ in range(n):
+    s = input()
+    if s == "ENTER":
+        result += len(set_)
+        set_.clear()
+    else:
+        set_.add(s)
+
+result += len(set_)
+print(result)
