4e128e13a12c048ea3a6c1390fe8e4231649c5b4
j30ngwoo <ttangle1234@naver.com>
2024-11-08 01:41:00 +0900

    2166 clear

---
+++ geometry/2166/2166.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    coords = []
+    for _ in range(n):
+        coords.append(list(map(int, input().split())))
+    
+    X = 0
+    Y = 1
+    result = 0
+    for i in range(-1, n - 1):
+        result += coords[i][X] * coords[i + 1][Y] 
+        result -= coords[i + 1][X] * coords[i][Y]
+    
+    print(f'{(abs(result) / 2):.1f}')
+
+if __name__ == "__main__":
+    main()
