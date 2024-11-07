3d9702208506e3ea1949916da80c3231e311e872
j30ngwoo <ttangle1234@naver.com>
2024-11-07 20:08:19 +0900

    2740 clear

---
+++ math/2740/2740.py
@@ -0,0 +1,24 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n, m = map(int, input().split())
+    a = []
+    for _ in range(n):
+        a.append(list(map(int, input().split())))
+
+    m, k = map(int, input().split())
+    b = []
+    for _ in range(m):
+        b.append(list(map(int, input().split())))
+
+    result = [[None for _ in range(k)] for _ in range(n)]
+    for y in range(n):
+        for x in range(k):
+            result[y][x] = sum(a[y][i] * b[i][x] for i in range(m))
+
+    for row in result:
+        print(' '.join(map(str, row)))
+
+if __name__ == "__main__":
+    main()
