622dae94c3347b73b384ea7fee434179e0f63489
j30ngwoo <ttangle1234@naver.com>
2024-11-09 23:23:06 +0900

    11478 clear

---
+++ data_structure/11478/11478.py
@@ -0,0 +1,15 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    s = input()
+    set_ = set()
+    
+    for l in range(len(s)):
+        for r in range(l + 1, len(s) + 1):
+            set_.add(s[l:r])
+
+    print(len(set_))
+
+if __name__ == "__main__":
+    main()
