6e3cc37e795792552ee65d942724a25bf22e021f
j30ngwoo <ttangle1234@naver.com>
2024-11-10 18:24:26 +0900

    1300 clear

---
+++ binary_search/1300/1300.py
@@ -0,0 +1,27 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    k = int(input())
+
+    def get_k(x):
+        result = 0
+        for i in range(1, min(n, x) + 1):
+            result += min(n, x // i)
+        return result
+
+    l = 1
+    r = k
+    while l < r:
+        # b[k] == x
+        x = (l + r) // 2
+        if k <= get_k(x):
+            r = x
+        else:
+            l = x + 1
+    
+    print(l)
+
+if __name__ == "__main__":
+    main()
