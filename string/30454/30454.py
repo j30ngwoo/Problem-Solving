9108f6e6623dedf5244421e4961a2c75ddcbf307
j30ngwoo <ttangle1234@naver.com>
2024-11-15 15:48:30 +0900

    30454 clear

---
+++ string/30454/30454.py
@@ -0,0 +1,25 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    N, _ = map(int, input().split())
+
+    max_ = 0
+    max_count = 0
+    for _ in range(N):
+        s = input().split('0')
+        lines = 0
+        for e in s:
+            if e:
+                lines += 1
+
+        if max_ < lines:
+            max_count = 1
+            max_ = lines
+        elif max_ == lines:
+            max_count += 1
+
+    print(max_, max_count)
+
+if __name__ == "__main__":
+    main()
