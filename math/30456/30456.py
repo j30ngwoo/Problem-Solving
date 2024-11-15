e409782336b7e3afe1e3a551d45dd72c16bde9c2
j30ngwoo <ttangle1234@naver.com>
2024-11-15 16:06:48 +0900

    30456 clear

---
+++ math/30456/30456.py
@@ -0,0 +1,9 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    N, L = map(int, input().split())
+    print('1' * (L - 1) + str(N))
+
+if __name__ == "__main__":
+    main()
