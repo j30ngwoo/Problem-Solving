06a739356440beb0ae326f447ba43f39c7af9eb7
j30ngwoo <ttangle1234@naver.com>
2024-11-15 16:17:33 +0900

    30458 clear

---
+++ string/30458/30458.py
@@ -0,0 +1,23 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    N = int(input())
+    S = input()
+    
+    dict_ = dict()
+    for i in range(N):
+        if N % 2 == 1 and N // 2 == i:
+            continue
+        if dict_.get(S[i]):
+            dict_[S[i]] += 1
+        else:
+            dict_[S[i]] = 1
+
+    if all(v % 2 == 0 for v in dict_.values()):
+        print('Yes')
+    else:
+        print('No')
+
+if __name__ == "__main__":
+    main()
