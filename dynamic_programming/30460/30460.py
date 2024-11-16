e55f4ef8567291d34ac68f64572315ca2ebf4b30
j30ngwoo <ttangle1234@naver.com>
2024-11-16 17:03:56 +0900

    30460 clear

---
+++ dynamic_programming/30460/30460.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    arr = [0] + [*map(int, input().split())] + [0, 0]
+    dp = [0 for _ in range(n + 3)]
+
+    dp[1] = arr[1]
+    dp[2] = dp[1] + arr[2]
+
+    for i in range(3, n + 3):
+        off = dp[i - 1] + arr[i]
+        on = dp[i - 3] + sum(arr[(i - 2):(i + 1)]) * 2
+        dp[i] = max(on, off)
+
+    print(dp[n + 2])
+
+if __name__ == "__main__":
+    main()
