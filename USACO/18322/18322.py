7b011420629f4404074ea973e6c87179ae1be121
j30ngwoo <ttangle1234@naver.com>
2024-11-16 12:20:53 +0900

    18322 clear

---
+++ USACO/18322/18322.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    N, K = map(int, input().split())
+    s = input().split()
+    
+    count = 0
+    for i in range(N):
+        if K < count + len(s[i]):
+            print()
+            count = 0
+        elif 0 < i:
+            print(end=' ')
+        
+        print(s[i], end='')
+        count += len(s[i])
+
+if __name__ == "__main__":
+    main()
