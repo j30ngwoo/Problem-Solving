6953449905c217addb3dbb2c63012c4ee281cd43
j30ngwoo <ttangle1234@naver.com>
2024-11-17 11:52:34 +0900

    1543 clear

---
+++ string/1543/1543.py
@@ -0,0 +1,19 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    s = input()
+    word = input()
+    result = 0
+    i = 0
+    while i < len(s) - len(word) + 1:
+        if s[(i):(i + len(word))] == word:
+            result += 1
+            i += len(word)
+        else:
+            i += 1
+    
+    print(result)
+
+if __name__ == "__main__":
+    main()
