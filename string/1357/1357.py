d8972d4bcadb3a6ea5b5a52675cd403b47ae7afd
j30ngwoo <ttangle1234@naver.com>
2024-11-17 15:45:15 +0900

    1357 clear

---
+++ string/1357/1357.py
@@ -0,0 +1,8 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    print(int(str(sum(map(lambda x: int(x[::-1]), input().split())))[::-1]))
+
+if __name__ == "__main__":
+    main()
