082f7cee4a8c5dd0bff8b64504b40550d7f7b721
j30ngwoo <ttangle1234@naver.com>
2024-11-17 19:35:29 +0900

    30464 clear

---
+++ dynamic_programming/30464/30464.py
@@ -0,0 +1,33 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    arr = [*map(int, input().split())]
+    dp = [0] * n
+
+    for i in range(n - 1, -1, -1):
+        next_i = i + arr[i]
+        if next_i == n - 1:
+            dp[i] = 1
+        elif next_i < n and dp[next_i] != 0:
+            dp[i] = dp[next_i] + 1
+    
+    for i in range(n - 1):
+        next_i = i - arr[i]
+        if 0 <= next_i and dp[next_i] != 0:
+            dp[i] = max(dp[i], dp[next_i] + 1)
+    
+    for i in range(n - 1, -1, -1):
+        next_i = i + arr[i]
+        if next_i < n - 1 and dp[next_i] != 0:
+            dp[i] = max(dp[i], dp[next_i] + 1)
+    
+    if dp[0] == 0:
+        print(-1)
+    else:
+        print(dp[0])
+
+if __name__ == "__main__":
+    main()
+
