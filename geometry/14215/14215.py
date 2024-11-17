83058a1a35c85843e6a42bb981c9eba20d1f6822
j30ngwoo <ttangle1234@naver.com>
2024-11-17 14:35:57 +0900

    14215 clear

---
+++ geometry/14215/14215.py
@@ -0,0 +1,14 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    arr = list(map(int, input().split()))
+    arr.sort()
+    
+    if arr[0] + arr[1] <= arr[2]:
+        print((arr[0] + arr[1]) * 2 - 1)
+    else:
+        print(sum(arr))
+
+if __name__ == "__main__":
+    main()
