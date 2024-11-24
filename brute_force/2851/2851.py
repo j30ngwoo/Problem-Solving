3fb1adcf17080617efe5efb51d864e67a34b4c09
j30ngwoo <ttangle1234@naver.com>
2024-11-24 17:46:28 +0900

    sort -> sorting

---
+++ brute_force/2851/2851.py
@@ -0,0 +1,17 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = 0
+    diff = 1000
+    result = 0
+    for i in range(10):
+        n += int(input())
+        if abs(n - 100) <= diff:
+            result = n
+            diff = abs(n - 100)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
