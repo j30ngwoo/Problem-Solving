dcbf88333c837989c3d97b4bb3705761683cd553
j30ngwoo <ttangle1234@naver.com>
2024-11-24 12:24:40 +0900

    1547 clear

---
+++ 1547/1547.py
@@ -0,0 +1,17 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    m = int(input())
+    ball = 1
+    for _ in range(m):
+        a, b = map(int, input().split())
+        if a == ball:
+            ball = b
+        elif b == ball:
+            ball = a
+    
+    print(ball)
+
+if __name__ == "__main__":
+    main()
