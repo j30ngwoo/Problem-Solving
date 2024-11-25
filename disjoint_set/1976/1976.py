75a53163227f0a47d150b4016e3ee50e1b9cb453
j30ngwoo <ttangle1234@naver.com>
2024-11-26 02:07:57 +0900

    1976 clear

---
+++ disjoint_set/1976/1976.py
@@ -0,0 +1,36 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    m = int(input())
+    graph = []
+    for _ in range(n):
+        graph.append(list(map(int, input().split())))
+    
+    parent = [x for x in range(n + 1)]
+
+    def find(x):
+        if parent[x] == x:
+            return x
+        
+        parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(a, b):
+        parent[find(a)] = find(b)
+
+    for a in range(n):
+        for b in range(n):
+            if graph[a][b]:
+                union(a + 1, b + 1)
+
+    routes = list(map(int, input().split()))
+    root = find(routes[0])
+    if all(root == find(x) for x in routes):
+        print('YES')
+    else:
+        print('NO')
+
+if __name__ == "__main__":
+    main()
