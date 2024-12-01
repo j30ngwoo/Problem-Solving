6120be8c4b9635c8776a8928fe537db4d118f322
j30ngwoo <ttangle1234@naver.com>
2024-12-01 15:22:07 +0900

    9093 clear

---
+++ string/9093/9093.py
@@ -0,0 +1,8 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    for _ in range(int(input())): print(' '.join(map(lambda s: s[::-1], input().split())))
+
+if __name__ == "__main__":
+    main()
