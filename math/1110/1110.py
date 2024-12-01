ee46582882b3abd862d5d2700e768b3d9716c2ad
j30ngwoo <ttangle1234@naver.com>
2024-12-01 12:01:34 +0900

    1110 clear

---
+++ math/1110/1110.py
@@ -0,0 +1,16 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    result = 0
+    temp = n
+    while True:
+        temp = temp % 10 * 10 + (temp // 10 + temp % 10) % 10
+        result += 1
+        if temp == n:
+            break
+    print(result)
+
+if __name__ == "__main__":
+    main()
