772508a699c584abd5b7bd9b77cf7a8bb42dc811
j30ngwoo <ttangle1234@naver.com>
2024-12-02 10:37:12 +0900

    2631 clear

---
+++ dynamic_programming/2631/2631.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr.append(int(input()))
+
+    counts = [1] * n
+    for now in range(n):
+        for prev in range(now):
+            if arr[prev] < arr[now]:
+                counts[now] = max(counts[prev] + 1, counts[now])
+    
+    print(n - max(counts))
+
+
+if __name__ == "__main__":
+    main()
