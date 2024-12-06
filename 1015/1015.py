5d7026b21a5e1a67d5288e5fe6bf56f5f29cc163
j30ngwoo <ttangle1234@naver.com>
2024-12-06 23:27:41 +0900

    1015 clear

---
+++ 1015/1015.py
@@ -0,0 +1,18 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = sorted(A)
+
+    result = []
+    for i in range(N):
+        B_index = B.index(A[i])
+        result.append(B_index)
+        B[B_index] = None
+    
+    print(' '.join(map(str, result)))
+
+if __name__ == "__main__":
+    main()
