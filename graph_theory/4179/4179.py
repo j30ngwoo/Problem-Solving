d1b36780204ed0a9413abd1b4fc1445f621786de
j30ngwoo <ttangle1234@naver.com>
2024-12-07 00:32:36 +0900

    4179 clear

--- graph_theory/4179/4179.py
+++ graph_theory/4179/4179.py
@@ -3,7 +3,7 @@ input = lambda: sys.stdin.readline().strip()
 from collections import deque
 from copy import deepcopy
 
-MAX = 10000
+INF = 10000
 
 def main():
     DX = [0, 0, 1, -1]
@@ -20,12 +20,12 @@ def main():
         for x in range(C):
             if grid[y][x] == 'J':
                 jihun_coord = [y, x]
-                grid[y][x] = MAX
+                grid[y][x] = INF
             elif grid[y][x] == 'F':
                 fire_coords.append([y, x])
-                grid[y][x] = MAX
+                grid[y][x] = INF
             elif grid[y][x] == '.':
-                grid[y][x] = MAX
+                grid[y][x] = INF
 
     exits = []
     j_grid = deepcopy(grid)
@@ -33,7 +33,7 @@ def main():
     q.append(jihun_coord + [0])
     while q:
         y, x, count = q.popleft()
-        if j_grid[y][x] != MAX:
+        if j_grid[y][x] != INF:
             continue
         j_grid[y][x] = count
         if y == 0 or y == R - 1 or x == 0 or x == C - 1:
@@ -42,7 +42,7 @@ def main():
         for d in range(4):
             next_y = y + DY[d]
             next_x = x + DX[d]
-            if 0 <= next_y < R and 0 <= next_x < C and j_grid[next_y][next_x] == MAX:
+            if 0 <= next_y < R and 0 <= next_x < C and j_grid[next_y][next_x] == INF:
                 q.append((next_y, next_x, count + 1))
 
     f_grid = deepcopy(grid)
@@ -51,14 +51,14 @@ def main():
         q.append(fire_coord + [0])
     while q:
         y, x, count = q.popleft()
-        if f_grid[y][x] != MAX:
+        if f_grid[y][x] != INF:
             continue
         f_grid[y][x] = count
 
         for d in range(4):
             next_y = y + DY[d]
             next_x = x + DX[d]
-            if 0 <= next_y < R and 0 <= next_x < C and f_grid[next_y][next_x] == MAX:
+            if 0 <= next_y < R and 0 <= next_x < C and f_grid[next_y][next_x] == INF:
                 q.append((next_y, next_x, count + 1))
     
     min_count = float('inf')
