2940d91d56300579d1fc90bfdfb8dd3fea01c80e
j30ngwoo <ttangle1234@naver.com>
2024-12-12 19:18:15 +0900

    7490 clear

---
+++ backtracking/7490/7490.py
@@ -0,0 +1,44 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    t = int(input())
+
+    def oper():
+        result = 0
+        value = n
+        for i in range(n - 2, -1, -1):
+            if opers[i] == ' ':
+                value += (10 ** len(str(value))) * (i + 1)
+            elif opers[i] == '+':
+                result += value
+                value = i + 1
+            else: # '-'
+                result -= value
+                value = i + 1
+        
+        result += value
+        return result
+
+    def recursion(depth):
+        if depth == n - 1:
+            if oper() == 0:
+                for i in range(n - 1):
+                    print(i + 1, end='')
+                    print(opers[i], end='')
+                print(n)
+            return
+
+        for o in [' ', '+', '-']:
+            opers.append(o)
+            recursion(depth + 1)
+            opers.pop()
+
+    for _ in range(t):
+        n = int(input())
+        opers = []
+        recursion(0)
+        print()
+
+if __name__ == "__main__":
+    main()
