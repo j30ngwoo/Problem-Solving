1147105db82eff22c9bffc3f55064a980b41f116
j30ngwoo <ttangle1234@naver.com>
2024-12-15 01:38:21 +0900

    2668 clear

---
+++ graph_theory/2668/2668.py
@@ -0,0 +1,27 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    arr = [0]
+    for _ in range(n):
+        arr.append(int(input()))
+    
+    result_list = []
+    for start in range(1, n + 1):
+        if start in result_list:
+            continue
+        temp_list = [start]
+        while True:
+            next = arr[temp_list[-1]]
+            if next in temp_list:
+                if next == start:
+                    result_list += temp_list
+                break
+            temp_list.append(next)
+    
+    print(len(result_list))
+    print('\n'.join(map(str, sorted(list(result_list)))))
+
+if __name__ == "__main__":
+    main()
