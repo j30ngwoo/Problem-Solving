9698df3dc99ceae228f68108a22940ebdedff179
j30ngwoo <ttangle1234@naver.com>
2024-12-16 23:53:46 +0900

    11021 clear

---
+++ math/11021/11021.py
@@ -0,0 +1,8 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    print('\n'.join(f"Case #{i}: {sum(map(int, input().split()))}" for i in range(1, int(input()) + 1)))
+
+if __name__ == "__main__":
+    main()
