5591075f3f43beb243e652470dcd78f1357e2b7b
j30ngwoo <ttangle1234@naver.com>
2024-12-21 13:05:13 +0900

    15591 clear

--- graph_theory/15591/15591.py
+++ graph_theory/15591/15591.py
@@ -2,6 +2,8 @@ import sys
 input = lambda: sys.stdin.readline().strip()
 sys.setrecursionlimit(100000)
 
+from collections import deque
+
 def main():
 	n, q = map(int, input().split())
 	graph = [[] for _ in range(n + 1)]
@@ -10,22 +12,37 @@ def main():
 		graph[a].append((b, w))
 		graph[b].append((a, w))
 	
-	def dfs(a, least):
-		nonlocal count, is_visited
-		if is_visited[a]:
-			return
-		is_visited[a] = True
-		if least >= k:
-			count += 1
-		for b, w in graph[a]:
-			if not is_visited[b]:
-				dfs(b, min(w, least))
+	# def dfs(a, least):
+	# 	nonlocal count, is_visited
+	# 	if is_visited[a]:
+	# 		return
+	# 	is_visited[a] = True
+	# 	if least >= k:
+	# 		count += 1
+	# 	for b, w in graph[a]:
+	# 		if not is_visited[b]:
+	# 			dfs(b, min(w, least))
+
+	def bfs(start):
+		nonlocal count
+		is_visited = [False] * (n + 1)
+		q = deque()
+		q.append((start, float('inf')))
+		while q:
+			a, least = q.popleft()
+			if is_visited[a]:
+				continue
+			is_visited[a] = True
+			if least >= k:
+				count += 1
+			for b, w in graph[a]:
+				if not is_visited[b]:
+					q.append((b, min(w, least)))
 
 	for _ in range(q):
 		k, v = map(int, input().split())
-		is_visited = [False] * (n + 1)
 		count = -1
-		dfs(v, float('inf'))
+		bfs(v)
 		print(count)
 
 main()
