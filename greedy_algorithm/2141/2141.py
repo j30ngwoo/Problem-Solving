8a51d80be39f1a0ec0eede46afaf405677ae3696
j30ngwoo <ttangle1234@naver.com>
2024-12-23 14:52:46 +0900

    2141 clear

---
+++ greedy_algorithm/2141/2141.py
@@ -0,0 +1,25 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+X = 0
+A = 1
+
+def main():
+    n = int(input())
+    towns = []
+    for _ in range(n):
+        towns.append(list(map(int, input().split())))
+    towns.sort()
+
+    left = 0
+    right = sum(town[A] for town in towns)
+    for i in range(n):
+        left += towns[i][A]
+        right -= towns[i][A]
+        if left >= right:
+            break
+
+    print(towns[i][X])
+
+if __name__ == "__main__":
+    main()
