b9e352312171094559ca8d6c2df1c6510e8b528e
j30ngwoo <ttangle1234@naver.com>
2024-12-24 15:10:20 +0900

    2154 clear

--- brute_force/2154/2154.py
+++ brute_force/2154/2154.py
@@ -3,13 +3,14 @@ input = lambda: sys.stdin.readline().strip()
 
 def main():
     n = int(input())
-    s = ''
+    s = '0'
     for i in range(1, n + 1):
         s += str(i)
-    i = 0
-    while s[i:(i + len(str(n)))] != str(n):
+    i = 1
+    str_n = str(n)
+    while s[i:(i + len(str_n))] != str_n:
         i += 1
-    print(i + 1)
+    print(i)
 
 if __name__ == "__main__":
     main()
