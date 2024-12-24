2c696ee7d9df22656b2a5a97b20ce6a6e918ca25
j30ngwoo <ttangle1234@naver.com>
2024-12-24 14:23:29 +0900

    2154 clear

---
+++ brute_force/2154/2154.py
@@ -0,0 +1,15 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    n = int(input())
+    s = ''
+    for i in range(1, n + 1):
+        s += str(i)
+    i = 0
+    while s[i:(i + len(str(n)))] != str(n):
+        i += 1
+    print(i + 1)
+
+if __name__ == "__main__":
+    main()
