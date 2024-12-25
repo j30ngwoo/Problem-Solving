a501aa1c549e41f3c57e939a51302a6957ec91b6
j30ngwoo <ttangle1234@naver.com>
2024-12-25 23:35:07 +0900

    5073 clear

---
+++ math/5073/5073.py
@@ -0,0 +1,21 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    while True:
+        arr = list(map(int, input().split()))
+        if 0 == arr[0] == arr[1] == arr[2]:
+            break
+
+        arr.sort()
+        if not arr[0] + arr[1] > arr[2]:
+            print('Invalid') 
+        elif arr[0] == arr[1] == arr[2]:
+            print('Equilateral')
+        elif arr[0] == arr[1] or arr[1] == arr[2]:
+            print('Isosceles')
+        else:
+            print('Scalene')
+        
+
+main()
