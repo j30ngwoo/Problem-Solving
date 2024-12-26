25a67eb38b8a84dec21eadfc983e79d26edaee32
j30ngwoo <ttangle1234@naver.com>
2024-12-26 11:25:13 +0900

    2531 clear

---
+++ two_pointer/2531/2531.py
@@ -0,0 +1,37 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    N, d, k, c = map(int, input().split())
+    sushis = []
+    for _ in range(N):
+        sushis.append(int(input()))
+    sushis *= 2
+    
+    table = dict()
+    for i in range(k):
+        if sushis[i] in table:
+            table[sushis[i]] += 1
+        else:
+            table[sushis[i]] = 1
+            
+    result = 0
+    for right in range(k, N + k):
+        if c not in table:
+            result = max(result, len(table) + 1)
+        else:
+            result = max(result, len(table))
+        left = right - k
+        if table[sushis[left]] == 1:
+            table.pop(sushis[left])
+        else:
+            table[sushis[left]] -= 1
+        if sushis[right] in table:
+            table[sushis[right]] += 1
+        else:
+            table[sushis[right]] = 1
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
