601b5368c26811822518b1977590aba1518030c0
j30ngwoo <ttangle1234@naver.com>
2024-12-27 03:13:00 +0900

    33062 clear

---
+++ USACO/33062/33062.py
@@ -0,0 +1,25 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    t = int(input())
+    base = [0, 0, 0]
+    for digit in range(1, 9):
+        base.append(base[-1] + int('5' * digit))
+
+    for _ in range(t):
+        n = int(input())
+        digit = len(str(n))
+        left = int('4' * digit)
+        right = 10 ** (digit - 1) * 5
+
+        if n <= left:
+            print(base[digit])
+        elif n < right:
+            print(base[digit] + n - left)
+        else:
+            print(base[digit + 1])
+
+
+if __name__ == "__main__":
+    main()
