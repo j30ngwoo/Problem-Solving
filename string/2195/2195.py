16b246a5ccbe958868f1e716a39b662852b01e18
j30ngwoo <ttangle1234@naver.com>
2024-12-28 13:58:54 +0900

    2195 clear

---
+++ string/2195/2195.py
@@ -0,0 +1,26 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    s = input()
+    p = input()
+
+    sub_strs = set()
+    for a in range(len(s)):
+        for b in range(a + 1, len(s) + 1):
+            sub_strs.add(s[a:b])
+    
+    start = 0
+    count = 0
+    while start < len(p):
+        count += 1
+        stop = start + 1
+        while stop < len(p) and p[start:stop + 1] in sub_strs:
+            stop += 1
+        start = stop
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
