022fbe817a96fdbc453afe89ad713ce1c9e01e33
j30ngwoo <ttangle1234@naver.com>
2025-01-02 18:46:58 +0900

    2133 clear

---
+++ dynamic_programming/2133/2133.py
@@ -0,0 +1,16 @@
+﻿import sys
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    counts = [1, 0]
+    for now in range(2, 31):
+        count = 0
+        for diff in range(4, now + 1, 2):
+            count += counts[now - diff] * 2
+        count += counts[now - 2] * 3
+        counts.append(count)
+    
+    print(counts[int(input())])
+
+if __name__ == "__main__":
+    main()
