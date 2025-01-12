388a0e8627c59c90fc18e3581f1607b409d6efa4
j30ngwoo <ttangle1234@naver.com>
2025-01-12 16:58:28 +0900

    2212 clear

---
+++ greedy_algorithm/2212/2212.py
@@ -0,0 +1,22 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    n = int(input())
+    k = int(input())
+    sensors = sorted(list(set(map(int, input().split()))))
+    
+    dists = []
+    for i in range(len(sensors) - 1):
+        dists.append(sensors[i + 1] - sensors[i])
+    dists.sort()
+
+    for _ in range(k - 1):
+        if dists:
+            dists.pop()
+
+    print(sum(dists))
+
+
+if __name__ == "__main__":
+    main()
