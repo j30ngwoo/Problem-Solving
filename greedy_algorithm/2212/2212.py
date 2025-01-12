cfdb2b9977ce079a18aa0b258f639b47051b4544
j30ngwoo <ttangle1234@naver.com>
2025-01-12 17:04:05 +0900

    2212 clear

--- greedy_algorithm/2212/2212.py
+++ greedy_algorithm/2212/2212.py
@@ -5,18 +5,7 @@ def main():
     n = int(input())
     k = int(input())
     sensors = sorted(list(set(map(int, input().split()))))
-    
-    dists = []
-    for i in range(len(sensors) - 1):
-        dists.append(sensors[i + 1] - sensors[i])
-    dists.sort()
-
-    for _ in range(k - 1):
-        if dists:
-            dists.pop()
-
-    print(sum(dists))
-
+    print(sum(sorted([sensors[i + 1] - sensors[i] for i in range(len(sensors) - 1)])[:len(sensors) - k]))
 
 if __name__ == "__main__":
     main()
