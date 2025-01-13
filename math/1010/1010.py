4387db08535336e28876509b39569dfa0b29001c
j30ngwoo <ttangle1234@naver.com>
2025-01-13 11:30:20 +0900

    1010 clear

---
+++ math/1010/1010.py
@@ -0,0 +1,16 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        arr = list(map(int, input().split()))
+        result = 1
+        for i in range(min(arr) + 1, max(arr) + 1):
+            result *= i
+        for i in range(2, max(arr) - min(arr) + 1):
+            result //= i
+        print(result)
+
+if __name__ == "__main__":
+    main()
