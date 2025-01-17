3be4fe5277294d6ed84515c5ec9a0c756dc7d0d6
j30ngwoo <ttangle1234@naver.com>
2025-01-17 20:35:01 +0900

    11657 clear

---
+++ dynamic_programming/11657/11657.py
@@ -0,0 +1,20 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+T = 0
+P = 1
+
+def main():
+    n = int(input())
+    arr = [tuple(map(int, input().split())) for _ in range(n)]
+    max_profit = [0] * (n + 1)
+
+    for day in range(n):
+        time, profit = arr[day]
+        for next_time in range(day + time, n + 1):
+            max_profit[next_time] = max(max_profit[next_time], max_profit[day] + profit)
+
+    print(max_profit[n]) 
+
+if __name__ == "__main__":
+    main()
