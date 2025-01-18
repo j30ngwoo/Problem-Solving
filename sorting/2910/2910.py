3ca6a0cdd4a0fba5140e4ad68892e11087134c12
j30ngwoo <ttangle1234@naver.com>
2025-01-18 19:27:11 +0900

    2910 clear

---
+++ sorting/2910/2910.py
@@ -0,0 +1,18 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    N, C = map(int, input().split())
+    arr = list(map(int, input().split()))
+    d = dict()
+    for e in arr:
+        if e in d:
+            d[e] += 1
+        else:
+            d[e] = 1
+    
+    for k, v in sorted(d.items(), key=lambda x: -x[1]):
+        print((str(k) + ' ') * v, end='')
+
+if __name__ == "__main__":
+    main()
