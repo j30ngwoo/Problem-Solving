458024a758f4487a4f86678e1af1be891abf7b31
j30ngwoo <ttangle1234@naver.com>
2025-01-24 13:13:23 +0900

    3109 clear

---
+++ graphs/3109/3109.py
@@ -0,0 +1,35 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    r, c = map(int, input().split())
+    grid = [list(input()) for _ in range(r)]
+
+    def dfs(y, x):
+        nonlocal is_found, result
+        
+        if is_found:
+            return
+        
+        grid[y][x] = 'x'
+
+        if x == c - 1:
+            result += 1
+            is_found = True
+            return
+        
+        for next_y in range(y - 1, y + 2):
+            if 0 <= next_y < r and grid[next_y][x + 1] == '.':
+                dfs(next_y, x + 1)       
+
+    result = 0
+    for y in range(r):
+        if grid[y][0] == 'x':
+            continue
+        is_found = False
+        dfs(y, 0)
+    
+    print(result)
+
+if __name__ == "__main__":
+    main()
