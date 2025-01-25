f1e1e679e9f5f1004b6e4d3b0e4267b809c7c09f
j30ngwoo <ttangle1234@naver.com>
2025-01-26 07:14:53 +0900

    3986 clear

--- data_structure/3986/3986.py
+++ data_structure/3986/3986.py
@@ -1,22 +1,27 @@
 ﻿import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+def is_goodword(s):
+    stk = []
+    for c in s:
+        if stk and stk[-1] == c:
+            stk.pop()
+        else:
+            stk.append(c)
+    
+    if stk:
+        return False
+    else:
+        return True
+
 def main():
     n = int(input())
 
     result = 0
     for _ in range(n):
-        s = input()
-        stk = []
-        for c in s:
-            if stk and stk[-1] == c:
-                stk.pop()
-            else:
-                stk.append(c)
-        
-        if not stk:
+        if is_goodword(input()):
             result += 1
-    
+
     print(result)
 
 if __name__ == "__main__":
