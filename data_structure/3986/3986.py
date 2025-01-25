fe7492ab1cabb00482bafe9dedc557654fe49894
j30ngwoo <ttangle1234@naver.com>
2025-01-25 13:22:31 +0900

    3986 clear

---
+++ data_structure/3986/3986.py
@@ -0,0 +1,23 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    n = int(input())
+
+    result = 0
+    for _ in range(n):
+        s = input()
+        stk = []
+        for c in s:
+            if stk and stk[-1] == c:
+                stk.pop()
+            else:
+                stk.append(c)
+        
+        if not stk:
+            result += 1
+    
+    print(result)
+
+if __name__ == "__main__":
+    main()
