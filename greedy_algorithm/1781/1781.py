9cf1c27b669f72261da41e970446a124ba51dd1f
j30ngwoo <ttangle1234@naver.com>
2025-01-28 20:00:52 +0900

    1781 clear

---
+++ greedy_algorithm/1781/1781.py
@@ -0,0 +1,21 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+from heapq import heappush, heappop 
+
+
+def main():
+    n = int(input())
+    noodles = [list(map(int, input().split())) for _ in range(n)]
+    noodles.sort()
+
+    pq = []
+    for deadline, count in noodles:
+        heappush(pq, count)
+        if deadline < len(pq):
+            heappop(pq)
+    
+    print(sum(pq))
+
+
+if __name__ == "__main__":
+    main()
