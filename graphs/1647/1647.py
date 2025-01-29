20642540ae84acf2a99f5da395c10863a22bbff1
j30ngwoo <ttangle1234@naver.com>
2025-01-29 19:12:34 +0900

    1647 clear

---
+++ graphs/1647/1647.py
@@ -0,0 +1,35 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+from heapq import heappop, heappush
+
+
+def main():
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a].append((c, b))
+        graph[b].append((c, a))
+
+    pq = [(0, 1)]
+    visited = set()
+    weights = []
+    while pq and len(visited) < n:
+        weight, node = heappop(pq)
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+        weights.append(weight)
+
+        for next_weight, next_node in graph[node]:
+            if next_node in visited:
+                continue
+            heappush(pq, (next_weight, next_node))
+
+    print(sum(weights) - max(weights))
+
+
+if __name__ == "__main__":
+    main()
