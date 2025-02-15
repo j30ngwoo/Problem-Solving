36b38bf2543498a219e504e5985b7a1f5f1682c3
j30ngwoo <ttangle1234@naver.com>
2025-02-15 21:03:23 +0900

    USACO/2024/dec/b3 clear

--- USACO/2024/dec/b3.py
+++ USACO/2024/dec/b3.py
@@ -14,7 +14,10 @@ def main():
                 moo[change] = new_alpha
                 moo = ''.join(moo)
                 if moo[0] != moo[1] == moo[2]:
-                    possibles[moo].append(i)
+                    if len(possibles[moo]) < 2:
+                        possibles[moo].append(i)
+                    else:
+                        possibles[moo][1] = i
 
     result = []
     for moo, idxs in moos.items():
@@ -23,7 +26,7 @@ def main():
         elif len(idxs) + 1 == f and possibles[moo]:
             if f == 1:
                 result.append(moo)
-            elif possibles[moo][0] <= idxs[0] - 3 or idxs[-1] + 3 <= possibles[moo][-1]:
+            elif possibles[moo][0] <= idxs[0] - 3 or idxs[-1] + 3 <= possibles[moo][1]:
                 result.append(moo)
 
     print(len(result))
