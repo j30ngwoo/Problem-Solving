3f664a038401479ff9058230de442b35ae1f900c
j30ngwoo <ttangle1234@naver.com>
2025-03-21 23:05:02 +0900

    수업

---
+++ 14916/14916.py
@@ -0,0 +1,16 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    n = int(input())
+    cnt2 = 0
+    while n >= 0 and n % 5 != 0:
+        n -= 2
+        cnt2 += 1
+    if n < 0:
+        print(-1)
+    else:
+        print(n // 5 + cnt2)
+
+if __name__ == "__main__":
+    main()
---
+++ 1789/1789.py
@@ -0,0 +1,18 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    s = int(input())
+    my_s = 0
+    i = 0
+    while my_s < s:
+        i += 1
+        my_s += i
+
+    if s < my_s:
+        print(i - 1)
+    else:
+        print(i)
+
+if __name__ == "__main__":
+    main()
