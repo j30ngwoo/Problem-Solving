4df3cde31298bfe20ba617bd5b23b6f460fdef1b
j30ngwoo <ttangle1234@naver.com>
2025-04-01 00:33:43 +0900

    1475 clear

---
+++ implementation/1475/1475.py
@@ -0,0 +1,16 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    l = [0] * 9
+    sn = 0
+    for e in map(int, input()):
+        if e == 6 or e == 9:
+            sn += 1
+        else:
+            l[e] += 1
+
+    print(max(l + [sn // 2 if sn % 2 == 0 else sn // 2 + 1]))
+
+if __name__ == "__main__":
+    main()
