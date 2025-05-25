9318d99c3747903b35c919d0ba9379599643de75
j30ngwoo <ttangle1234@naver.com>
2025-05-25 11:48:21 +0900

    1251 clear

---
+++ brute_force/1251/1251.py
@@ -0,0 +1,13 @@
+﻿import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    word = input()
+    result = []
+    for i in range(1, len(word) - 1):
+        for j in range(i + 1, len(word)):
+            result.append(word[:i][::-1] + word[i:j][::-1] + word[j:][::-1])
+    print(min(result))
+
+if __name__ == "__main__":
+    main()
